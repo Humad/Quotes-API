189c3d52ded300496920cb6b5da30a4fed63bbe4
Humad <humad@live.com>
2016-09-25 09:58:32 -0700

    Added dependencies

---
+++ index.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    connectionLimit: 100,
+    host: "humadshah.com",
+    user: "public_guests", // user with only the SELECT privilege 
+    password:"hello",
+    database:"my_quotes" // a database I've got up on my hosting site
+});
+
+connection.connect(function(error){
+    if(error){
+        console.log("Couldn't connect :(  " + error);
+    } else {
+        console.log("Connected successfully~!");
+    }    
+});
+
+connection.query("SELECT * FROM Quotes", function(error, rows, fields){
+   if (error) {
+       console.log("Something went wrong... " + error);
+   } else {
+      console.log(rows);
+      return rows;
+   }
+});
+
+connection.end();
