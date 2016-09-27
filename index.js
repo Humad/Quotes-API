724d51adc5ac3dcc4fa1d83d11971958f3e5ceae
Humad <humad@live.com>
2016-09-26 22:28:13 -0700

    Added ability to add quotes

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ var path = require("path");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 
+var connection = mysql.createConnection({
+    connectionLimit: 100,
+    host: "humadshah.com",
+    user: "public_guests",
+    password:"hello",
+    database:"my_quotes"
+});
+
+connection.connect(function(error){
+    if(error){
+        console.log("Couldn't connect :(  " + error);
+    } else {
+        console.log("Connected successfully~!");
+    }    
+});
+
 app.listen(port, function(){
     console.log("Listening on port " + port);
 });
@@ -15,31 +31,32 @@ app.get("/", function(request, response){
     response.render("index");
 });
 
-app.get("/get", function(request, response){
-    var connection = mysql.createConnection({
-        connectionLimit: 100,
-        host: "humadshah.com",
-        user: "public_guests",
-        password:"hello",
-        database:"my_quotes"
-    });
-    
-    connection.connect(function(error){
-        if(error){
-            console.log("Couldn't connect :(  " + error);
-        } else {
-            console.log("Connected successfully~!");
-        }    
-    });
-    
+app.get("/get", function(req, res){    
     connection.query("SELECT * FROM Quotes", function(error, rows, fields){
        if (error) {
            console.log("Something went wrong... " + error);
        } else {
-          response.jsonp({"quotes": rows});
+          res.jsonp({"quotes": rows});
        }
     });
     
     connection.end();
+});
+
+app.get("/add", function(req, res){
+    
+    connection.query("INSERT INTO `Quotes`(`ID`, `Quote`, `Author`) VALUES (0,'" + decodeURIComponent(req.query.quote.toString()) + "','" + decodeURIComponent(req.query.author.toString()) + "')", function(error, rows, fields){
+       if (error) {
+           console.log("Something went wrong... " + error);
+       } else {
+           console.log("Entry added!");
+       }
+    });
+    
+    res.end();
+});
+
+app.get("/secret", function(req, res){
+    res.render("addNewQuote");
 });
 
