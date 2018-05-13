ab93449b5c7e33a6b5676e065629ac2223e07150
Humad <humad@live.com>
2018-05-13 12:25:56 -0400

    updated files to match heroku version

--- index.js
+++ index.js
@@ -31,26 +31,6 @@ app.get("/get", function(req, res){
     })
 });
 
-// Gets a single quote
-app.get("/getOne", function(req, res) {
-    var mLabUri = "mongodb://" + process.env.readerId +
-        ":" + process.env.readerPass + "@ds061246.mlab.com:61246/projects";
-    mongo.connect(mLabUri, function(err, db){
-        if (err){
-            throw err;
-            res.end(err);
-        } else {
-            db.collection("quotes").find().toArray(function(err, docs){
-                var singleQuote = []
-                var rand = docs[Math.floor(Math.random() * docs.length)];
-                singleQuote.push(rand)
-                res.status(200).json({data : singleQuote});
-                db.close();
-            });
-        }
-    })
-});
-
 // Renders the addNewQuote page, allowing users to add new quotes
 app.get("/add", function(req, res){
     res.render("addNewQuote");
