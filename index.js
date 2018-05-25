51171779e4c66cbaa35f5b781554a673c8524f4e
Humad <humad@live.com>
2018-05-25 16:24:26 -0700

    added remote access header

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ setInterval(function() {
     http.get("http://getquote.herokuapp.com");
 }, 300000); // 5 Minutes 
 
+// Allow cross origin requests
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 // -- GET requests -- 
 
 // Renders the home page
